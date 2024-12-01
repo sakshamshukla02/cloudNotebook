ed8be6181784d50db4722b7618c1a3e053b6762f
sakshamshukla02 <sshuklaknp2002@gmail.com>
2024-12-01 11:04:24 +0530

    added start command

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,11 +11,11 @@ app.use(express.static("public"));
 
 app.use("/user",userRouter);
 
-app.get("/").get(async(req,res)=>{
+app.get("/",(async(req,res)=>{
     return res.status(200).json({
        message: "Hello User" 
     })
-})
+}))
 
 app.use((req,res,err,next)=>{
     throw new ApiError(500,"Internal Server Error");
